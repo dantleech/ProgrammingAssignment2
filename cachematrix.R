c2db18826cb62c5f142f4bf163a7e09af3cbe57c
dantleech <dan.t.leech@gmail.com>
2016-01-24 22:52:36 +0100

    Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions allow the result of `solve()` method to be associated with
+## an object which represents a "cached" matrix.
 
-## Write a short comment describing this function
+## The `makeCacheMatrix()` function returns a list of functions allowing the
+## matrix value to be gotten and provides methods to allow caching.
+## `setinv()` sets the `inv` variable in the parent scope. `getinv()`
+## returns this same variable.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(m = matrix()) {
+    inv <- NULL
+    get <- function () m
+    setinv <- function(inv) {
+        inv <<- inv
+    }
+    getinv <- function () inv
+    list(get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+# The `cacheSolve()` function determines if the inverse has already been set
+# on the given "cached matrix" (as returned from the function above), it it
+# has already been set (it is not NULL) then it is returned, otherwise the
+# value is computed and `setinv()` is called to cache the value for subsequent
+# calls.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(cm) {
+    inv = cm$getinv()
+
+    if (!is.null(inv)) {
+        message("Getting inversed matrix")
+        return(inv)
+    }
+
+    m <- cm$get()
+    cm$setinv(solve(m))
+    cm$getinv()
 }
